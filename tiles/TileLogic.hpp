756ce9abf93dd0fd7381427c79d8307d0fbf0eab
kafadev <89330010+kafadev@users.noreply.github.com>
2025-04-01 18:04:45 -0700

    Added empire struct

---
+++ tiles/TileLogic.hpp
@@ -0,0 +1,18 @@
+/* Logic.hpp */
+
+struct Empire {
+    int food;               /* Used to calculate growth */
+    int population;         /* Used to calculate growth */
+    int number_of_towns;    /* Used for influence multiplier */
+    int minerals;           /* Used for military strength */
+};
+
+class TileLogic {
+    public:
+        ~TileLogic();
+
+        TileLogic();
+
+    private:
+
+};
